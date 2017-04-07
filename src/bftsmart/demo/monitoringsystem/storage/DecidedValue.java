6f0e5992bea1ca5bbafbf3afe1c2ef8e8be961ca
BernardoPalma <bernardo.palma@tecnico.ulisboa.pt>
2017-04-07 19:15:11 +0100

    started base for TSD

---
+++ src/bftsmart/demo/monitoringsystem/storage/DecidedValue.java
@@ -0,0 +1,19 @@
+package bftsmart.demo.monitoringsystem.storage;
+
+public class DecidedValue {
+    private final String type;
+    private final Object value;
+
+    public DecidedValue(String type, Object value){
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
