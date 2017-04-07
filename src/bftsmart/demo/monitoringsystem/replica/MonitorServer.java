8a1e20870ab63a412ee7dcd4069df8700fbdc401
BernardoPalma <bernardo.palma@tecnico.ulisboa.pt>
2017-04-07 19:01:33 +0100

    integration beginning

--- src/bftsmart/demo/monitoringsystem/replica/MonitorServer.java
+++ src/bftsmart/demo/monitoringsystem/replica/MonitorServer.java
@@ -10,18 +10,58 @@ import bftsmart.tom.MessageContext;
 import bftsmart.tom.ServiceReplica;
 import bftsmart.tom.server.defaultservices.DefaultRecoverable;
 import bftsmart.tom.server.defaultservices.DefaultReplier;
+import pt.pasadinhas.App;
+import pt.pasadinhas.adaptation.Adaptation;
+import pt.pasadinhas.adaptation.AdaptationInstance;
+import pt.pasadinhas.component.Component;
+import pt.pasadinhas.goals.MinimizationGoal;
+import pt.pasadinhas.storage.Storage;
+import pt.pasadinhas.communication.ServerReplica;
+import pt.pasadinhas.engine.Engine;
+import pt.pasadinhas.parser.PolicyFileParser;
+import udojava.evalex.Expression;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class MonitorServer extends DefaultRecoverable {
 
     ServiceReplica replica;
     private Aggregator aggregator;
+    private int snapshotCounter = 0;
+    private int counter = 0;
+    String currentLeader = "0";
 
     public MonitorServer(int id){
         Map<String, Sensor> sensors = SensorLoadingUtil.loadSensors();
         aggregator = sensors != null ? new Aggregator(sensors) : new Aggregator();
         replica = new ServiceReplica(id, "monitor-config", this, this, null, new DefaultReplier());
+        Expression.init();
+        System.out.println("WERGSDFGSERGWEGE" + App.class.getClassLoader().getResource("Test.policy").getFile());
+        //PolicyFileParser.parse("Test.policy", Engine.getInstance());
+        Engine.register(new Component("replica"));
+        Engine.register(new MinimizationGoal(new Expression("latency")));
+        Adaptation adaptation = new Adaptation("NewLeader") {
+            @Override
+            public void execute(Component.Instance... instances) {
+                Engine.getInstance().sendReconfiguration("switchleader", instances);
+            }
+        };
+        adaptation.addImpactFunction("latency", new Expression(":r.Latency"));
+        adaptation.addInputVariable("replica", "r");
+        Engine.register(adaptation);
+        Engine.component("replica").addInstance("0");
+        Engine.component("replica").addInstance("1");
+        Engine.component("replica").addInstance("2");
+        Engine.component("replica").addInstance("3");
+        try {
+            Engine.register(new ServerReplica("0", "192.168.56.10", 12000));
+            Engine.register(new ServerReplica("1", "192.168.56.11", 12010));
+            Engine.register(new ServerReplica("2", "192.168.56.12", 12020));
+            Engine.register(new ServerReplica("3", "192.168.56.13", 12030));
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -75,6 +115,29 @@ public class MonitorServer extends DefaultRecoverable {
     }
 
     private void handleDecidedValue(String type, Object value){
-        System.out.println("Decided value for type: " + type + ". Obtained: " + value.toString());
+        snapshotCounter++;
+        System.out.println("Decided value in snapshot " + snapshotCounter + " for type: " + type + ". Obtained: " + value.toString());
+
+        if(type.equals("Latency")){
+            System.out.println("Im IN");
+            BigDecimal[] v = (BigDecimal[]) value;
+            int i = 0;
+            for (BigDecimal vl: v) {
+                Storage.put("replica." + i + ".Latency", vl);
+                i++;
+            }
+
+            if(counter++%20 == 0) {
+                AdaptationInstance adaptationInstance = Engine.getInstance().run();
+                if (adaptationInstance.instances[0].getId().equals(currentLeader)) {
+                    return;
+                }
+                adaptationInstance.execute();
+                currentLeader = adaptationInstance.instances[0].getId();
+            }
+
+        }
+
+
     }
 }
---
+++ src/bftsmart/demo/monitoringsystem/storage/TSDatabase.java
@@ -0,0 +1,14 @@
+package bftsmart.demo.monitoringsystem.storage;
+
+public class TSDatabase {
+
+
+    public void insertValue(String type, long timestamp){
+
+    }
+
+    public void selectValues(String type, int start, int amount){
+
+    }
+
+}
